cee0c173096f30c1fba73ccbed36bcd25b2b4edc
Konstantin <90984821+KotSka666@users.noreply.github.com>
2024-12-02 09:06:27 +0300

    Add files via upload

---
+++ lesson7_6.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+
+directory = r'C:\lesson_Un\modul_7\lesson_7_6'
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        # Формируем полный путь к файлу
+        filepath = os.path.join(root, file)
+
+        # Получаем время последнего изменения файла
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+
+        # Получаем размер файла
+        filesize = os.path.getsize(filepath)
+
+        # Получаем родительскую директорию файла
+        parent_dir = os.path.dirname(filepath)
+
+        # Вывод информации о файле
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
